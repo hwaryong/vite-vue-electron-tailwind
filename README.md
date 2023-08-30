cf351ef412c2f054fa0a2e554d3afd0035bd2eda
dayliver <dayliver@gmail.com>
2023-08-30 16:01:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# vite-vue-electron-tailwind
+
+An Electron application with Vue
+
+## Recommended IDE Setup
+
+- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+## Project Setup
+
+### Install
+
+```bash
+$ npm install
+```
+
+### Development
+
+```bash
+$ npm run dev
+```
+
+### Build
+
+```bash
+# For windows
+$ npm run build:win
+
+# For macOS
+$ npm run build:mac
+
+# For Linux
+$ npm run build:linux
+```
---
+++ src/renderer/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+const powers = [
+    { name: 'Vite', url: 'https://vitejs.dev/' },
+    { name: 'Vue', url: 'https://vuejs.org/' },
+    { name: 'Electron', url: 'https://www.electronjs.org/' },
+    { name: 'Tailwind CSS', url: 'https://tailwindcss.com/' },
+];
+</script>
+
+<template>
+    <div class="w-full h-screen flex bg-green-100 text-green-900">
+        <div class="m-auto flex gap-8">
+            <a
+                v-for="power of powers"
+                :href="power.url"
+                class="text-3xl font-bold"
+            >{{ power.name }}</a>
+        </div>
+    </div>
+</template>
